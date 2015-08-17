241369fa6577414fbabb725f0351c8400cee757e
Tein van der Lugt <teinvdlugt@gmail.com>
2015-08-17 16:22:29 +0200

    Add resolution and precision controls

--- app/src/main/java/com/teinvdlugt/android/fractals/FractalView.java
+++ app/src/main/java/com/teinvdlugt/android/fractals/FractalView.java
@@ -8,40 +8,57 @@ import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
+import android.widget.ProgressBar;
 
 public class FractalView extends View {
 
-    private double range = 2.7;
-    private int widthPixels = 1024;
-    private boolean[][] pixels = new boolean[widthPixels][widthPixels];
+    private double range = 3.5;
+    private int resolution = 512;
+    private int precision = 1000;
+    private ProgressBar progressBar;
+    private boolean[][] pixels = new boolean[resolution][resolution];
 
     Paint paint = new Paint();
 
     public void recalculate() {
+        final int finalResolution = resolution;
+        final int finalPrecision = precision;
         new Thread(new Runnable() {
             @Override
             public void run() {
                 for (int y = 0; y < pixels.length; y++) {
                     for (int x = 0; x < pixels[y].length; x++) {
-                        double cReal = -range / 2 + range / widthPixels * x;
-                        double cImg = -range / 2 + range / widthPixels * y;
+                        double cReal = -range / 2 + range / finalResolution * x;
+                        double cImg = -range / 2 + range / finalResolution * y;
                         double zReal = cReal, zImg = cImg;
 
                         int i = 0;
-                        int max = 1000;
-                        while (zReal * zReal + zImg * zImg <= 4 && i < max) {
+                        while (zReal * zReal + zImg * zImg <= 4 && i < finalPrecision) {
                             double zRealNew = zReal * zReal - zImg * zImg + cReal;
                             zImg = 2 * zReal * zImg + cImg;
                             zReal = zRealNew;
                             i++;
                         }
-                        pixels[y][x] = i == max;
+                        pixels[y][x] = i == finalPrecision;
                     }
-                    Log.d("coffee", "row " + y + " " + pixels[y][40]);
-                    if (y % widthPixels / 8 == 0) postInvalidate();
+                    //Log.d("coffee", "row " + y + " " + pixels[y][40]);
+
+                    final int finalY = y;
+                    progressBar.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            progressBar.setProgress(finalY);
+                        }
+                    });
                 }
 
                 postInvalidate();
+                progressBar.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressBar.setProgress(0);
+                    }
+                });
             }
         }).start();
     }
@@ -50,7 +67,10 @@ public class FractalView extends View {
     protected void onDraw(Canvas canvas) {
         paint.setColor(Color.BLACK);
 
-        float pixelsPerBlock = getWidth() / (float) widthPixels;
+        float pixelsPerBlockX = getWidth() / (float) resolution;
+        float pixelsPerBlockY = getHeight() / (float) resolution;
+        float pixelsPerBlock = pixelsPerBlockX < pixelsPerBlockY ? pixelsPerBlockX : pixelsPerBlockY;
+
         for (int y = 0; y < pixels.length; y++) {
             for (int x = 0; x < pixels[y].length; x++) {
                 if (pixels[y][x]) {
@@ -61,9 +81,37 @@ public class FractalView extends View {
         }
 
         // Draw y axis
-        canvas.drawLine(widthPixels / 2 * pixelsPerBlock, 0, widthPixels / 2 * pixelsPerBlock, getHeight(), paint);
+        canvas.drawLine(resolution / 2 * pixelsPerBlock, 0, resolution / 2 * pixelsPerBlock, getHeight(), paint);
         // Draw x axis
-        canvas.drawLine(0, widthPixels / 2 * pixelsPerBlock, getWidth(), widthPixels / 2 * pixelsPerBlock, paint);
+        canvas.drawLine(0, resolution / 2 * pixelsPerBlock, getWidth(), resolution / 2 * pixelsPerBlock, paint);
+    }
+
+
+    public ProgressBar getProgressBar() {
+        return progressBar;
+    }
+
+    public void setProgressBar(ProgressBar progressBar) {
+        this.progressBar = progressBar;
+        progressBar.setMax(resolution);
+    }
+
+    public int getResolution() {
+        return resolution;
+    }
+
+    public void setResolution(int resolution) {
+        this.resolution = resolution;
+        progressBar.setMax(resolution);
+        pixels = new boolean[resolution][resolution];
+    }
+
+    public int getPrecision() {
+        return precision;
+    }
+
+    public void setPrecision(int precision) {
+        this.precision = precision;
     }
 
     public FractalView(Context context) {
--- app/src/main/java/com/teinvdlugt/android/fractals/MainActivity.java
+++ app/src/main/java/com/teinvdlugt/android/fractals/MainActivity.java
@@ -1,16 +1,53 @@
 package com.teinvdlugt.android.fractals;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    FractalView fractalView;
+    DrawerLayout drawerLayout;
+    LinearLayout drawer;
+    EditText resolutionET, precisionET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FractalView fractalView = (FractalView) findViewById(R.id.fractalView);
+        initViews();
+
+        resolutionET.setText(fractalView.getResolution() + "");
+        precisionET.setText(fractalView.getPrecision() + "");
+
+        fractalView.setProgressBar((ProgressBar) findViewById(R.id.progressBar));
+        fractalView.recalculate();
+    }
+
+    private void initViews() {
+        fractalView = (FractalView) findViewById(R.id.fractalView);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        drawer = (LinearLayout) findViewById(R.id.drawer);
+        resolutionET = (EditText) findViewById(R.id.resolution);
+        precisionET = (EditText) findViewById(R.id.precision);
+    }
+
+    public void onClickApply(View view) {
+        try {
+            int resolution = Integer.parseInt(resolutionET.getText().toString());
+            fractalView.setResolution(resolution);
+        } catch (NumberFormatException ignored) {/*ignored*/}
+        try {
+            int precision = Integer.parseInt(precisionET.getText().toString());
+            fractalView.setPrecision(precision);
+        } catch (NumberFormatException ignored) {/*ignored*/}
+
+        drawerLayout.closeDrawer(drawer);
         fractalView.recalculate();
     }
 }
